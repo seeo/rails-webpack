73e59b9aa0c5d60dbb09cb610625e2315f005995
seeo <aragonlotr5@gmail.com>
2019-05-21 19:56:32 +0800

    fixed the AJX response handler within the getPost function. Also, Htat Win recommend using fetch method instead. Does the same thing with fewer lines of code.

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.getPost = this.getPost.bind(this);
+    }
+
+    state = {
+        stuff: "",
+        id: ""
+    }
+
+    componentDidMount(){
+        this.getPost(1)
+    }
+
+    getPost(id){
+        fetch(`/posts/${id}.json`)
+            .then(function(res){return res.json()})
+            .then(function(pikachu){console.log(pikachu.name)})
+
+    }
+
+    getPostBackUp(id) {
+
+        let reactThis = this;
+
+        function responseHandler (event){
+            console.log("HERE LA" ,this)
+
+            var response = JSON.parse(this.responseText);
+
+       //     var res = JSON.stringify(response);
+
+            console.log(response)
+            reactThis.setState({ stuff: response });
+        };
+
+        var request = new XMLHttpRequest();
+
+        request.addEventListener("load", responseHandler);
+
+        request.open("GET", `/posts/${id}.json`);
+
+        request.send();
+
+
+
+
+    }
+
+
 
   render(){
     return(<div>
             <h1>APPPPPPP!</h1>
             <h2>this is being rendered by React in the components folder, the app.jsx file.</h2>
+            <div>{this.state.stuff.name}</div>
           </div>);
   }
 }
