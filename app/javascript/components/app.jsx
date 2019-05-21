f536bbb8f51d825474b075c776307d37a48f0543
seeo <aragonlotr5@gmail.com>
2019-05-21 16:04:38 +0800

    added some text in the app.jsx to say how the text is being rendered

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -5,6 +5,7 @@ export default class App extends React.Component{
   render(){
     return(<div>
             <h1>APPPPPPP!</h1>
+            <h2>this is being rendered by React in the components folder, the app.jsx file.</h2>
           </div>);
   }
 }
