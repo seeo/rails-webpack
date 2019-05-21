9822dfe33a0a1b2bf866fc1d2e3c0761f28f1dba
seeo <aragonlotr5@gmail.com>
2019-05-21 22:42:09 +0800

    both XMLHttpRequest and fetch methods can do AJAX request. in app.jsx

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -4,35 +4,45 @@ export default class App extends React.Component{
     constructor(props){
         super(props);
         this.getPost = this.getPost.bind(this);
+        // this.getPostBackUp = this.getPostBackUp.bind(this);
     }
 
     state = {
         stuff: "",
-        id: ""
+        id: "",
+        title: "",
     }
 
     componentDidMount(){
-        this.getPost(1)
+        this.getPost(1);
+        // this.getPostBackUp(1);
     }
 
     getPost(id){
+        let res;
         fetch(`/posts/${id}.json`)
-            .then(function(res){return res.json()})
-            .then(function(pikachu){console.log(pikachu.name)})
+            .then(response => {
+                console.log("printing response", response);
+                return response.json();
+            })
+            .then(json => {
+                console.log("printing json", json);
+                this.setState({
+                    stuff: json,
+                })
+            })
 
-    }
+                }
+            // this.setState({ stuff: res });
 
-    getPostBackUp(id) {
+    getPostBackUp(id){
 
         let reactThis = this;
 
         function responseHandler (event){
-            console.log("HERE LA" ,this)
-
+            console.log("HERE LA" ,this);
             var response = JSON.parse(this.responseText);
-
        //     var res = JSON.stringify(response);
-
             console.log(response)
             reactThis.setState({ stuff: response });
         };
@@ -44,12 +54,7 @@ export default class App extends React.Component{
         request.open("GET", `/posts/${id}.json`);
 
         request.send();
-
-
-
-
     }
-
 
 
   render(){
